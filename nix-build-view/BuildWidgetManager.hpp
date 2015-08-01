0254a86b604395f6e843f8d62a0f0bec6cf85c77
Matthias Beyer <mail@beyermatthias.de>
2015-08-01 19:14:13 +0200

    Formatted: BuildWidgetManager.hpp

--- nix-build-view/BuildWidgetManager.hpp
+++ nix-build-view/BuildWidgetManager.hpp
@@ -4,16 +4,27 @@
 #include "WidgetManagerBase.hpp"
 
 class BuildWidgetManager : public WidgetManagerBase {
+
 public:
+
     int type() const;
+
     static BuildWidgetManager* Instance();
-    void addBuild(std::string UUID, std::vector< std::string > phases);
+
+    void addBuild(std::string UUID, std::vector<std::string> phases);
+
     void removeBuild(std::string UUID);
+
     int getPhase(std::string UUID);
+
     void setPhase(std::string UUID, int phase);
+
 private:
+
     BuildWidgetManager();
+
     void sort();
+
 };
 
 #endif // BUILDWIDGETMANAGER_H
