65fdaac84608fc40ad42eb58a1b9fc7314af9874
Matthias Beyer <mail@beyermatthias.de>
2015-08-01 19:53:05 +0200

    Formatted: TerminalWidget.hpp

--- nix-build-view/TerminalWidget.hpp
+++ nix-build-view/TerminalWidget.hpp
@@ -9,29 +9,42 @@
 
 #include "Widget.hpp"
 
+class TerminalWidget : public Widget
+{
 
-class TerminalWidget : public Widget {
 public:
+
     AdvancedStringContainer render(unsigned int width, unsigned int height);
+
     void append(AdvancedStringContainer line);
+
     int type() const;
+
     void keyboardInputHandler(int ch);
+
     AdvancedStringContainer log() {
         return m_logfile;
-    };
+    }
+
     static TerminalWidget* Instance();
+
 private:
+
     AdvancedStringContainer m_logfile;
     unsigned int m_line = 0;
     std::vector<AdvancedStringContainer> m_terminal;
+
     unsigned int width() {
         return m_width;
     }
+
     unsigned int height() {
         return m_height;
     }
-    unsigned int m_width = 0;
-    unsigned int m_height = 0;
+
+    unsigned int m_width    = 0;
+    unsigned int m_height   = 0;
+
 };
 
-#endif
+#endif // TERMINALWIDGET__HPP
