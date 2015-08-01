f5ee17cce4791342e983cdcd315637a8b72f123c
Matthias Beyer <mail@beyermatthias.de>
2015-08-01 19:26:04 +0200

    Formatted: HelpWidget.hpp

--- nix-build-view/HelpWidget.hpp
+++ nix-build-view/HelpWidget.hpp
@@ -5,19 +5,28 @@
 #include "Widget.hpp"
 
 class HelpWidget : public Widget {
+
     AdvancedStringContainer render(unsigned int width, unsigned int height);
+
     int type() const;
+
 private:
-  std::string p(std::string s);
-      unsigned int width() {
+
+    std::string p(std::string s);
+
+    unsigned int width() {
         return m_width;
     }
+
     unsigned int height() {
         return m_height;
     }
-    unsigned int m_width = 0;
+
+    unsigned int m_width  = 0;
     unsigned int m_height = 0;
+
     std::vector<AdvancedStringContainer> m_terminal;
+
 };
 
 #endif //HELPWIDGET__HPP
