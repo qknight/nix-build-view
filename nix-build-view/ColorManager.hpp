ee950c9ba5ca7ef42681b12332b8275792fda655
Matthias Beyer <mail@beyermatthias.de>
2015-08-01 19:16:54 +0200

    Formatted: ColorManager.hpp

--- nix-build-view/ColorManager.hpp
+++ nix-build-view/ColorManager.hpp
@@ -4,12 +4,18 @@
 #include <map>
 #include <string>
 
-// ncurses abstraction to make transparent use of attron(COLOR_PAIR(cm.setColor(color1, color2))) 
+// ncurses abstraction to make transparent use of
+// attron(COLOR_PAIR(cm.setColor(color1, color2)))
 class ColorManager {
+
 public:
+
     int setColor(int bg, int fg);
+
 private:
+
     std::map<std::string, int> m_map;
+
 };
 
 #endif // COLORMANAGER_HPP
