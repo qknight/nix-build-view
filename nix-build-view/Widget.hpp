c15e2fdcee7ff25abd4ea1ab8134ffa0548f0181
Matthias Beyer <mail@beyermatthias.de>
2015-08-01 19:56:33 +0200

    Formatted: Widget.hpp

--- nix-build-view/Widget.hpp
+++ nix-build-view/Widget.hpp
@@ -5,30 +5,39 @@
 #include "AdvancedString.hpp"
 
 namespace WidgetName {
-enum {
-    HelpWidget,
-    TerminalWidget,
-    FetchWidget,
-    BuildWidget,
-    StatusWidget,
-    VerticalSpacerWidget,
-    FetchWidgetManager,
-    BuildWidgetManager,
-};
+
+    enum {
+        HelpWidget,
+        TerminalWidget,
+        FetchWidget,
+        BuildWidget,
+        StatusWidget,
+        VerticalSpacerWidget,
+        FetchWidgetManager,
+        BuildWidgetManager,
+    };
+
 };
 
 class Widget {
+
 public:
-    virtual AdvancedStringContainer render(unsigned int width, unsigned int height) = 0;
+
+    virtual AdvancedStringContainer render(unsigned int width,
+                                           unsigned int height) = 0;
+
     void update() {
         WindowManager::Instance()->update(this);
     }
+
     virtual int type() const = 0;
     virtual void keyboardInputHandler(int ch) {};
+
     // for nested widget rendering
     virtual unsigned int rowsWantedByWidget() {
         return 1;
     }
+
 };
 
-#endif
+#endif // WIDGET__HPP
